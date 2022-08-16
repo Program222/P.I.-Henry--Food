b4f6a5e091cc60065f9eb154c896429adf8e3ed9
Program222 <j_medina19@outlook.com>
2022-08-16 12:53:28 -0400

    end models

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -2,11 +2,53 @@ const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
-  // defino el modelo
-  sequelize.define('recipe', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  });
+    // defino el modelo
+    sequelize.define('recipe', {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            allowNull: false,
+            primaryKey: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        summary: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        healthScore: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                min: 0,
+                max: 100,
+            }
+        },
+        image: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        steps: {
+            type: DataTypes.TEXT,
+        },
+        score: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            validate: {
+                min: 0,
+                max: 100, 
+            }
+        },
+        dishTypes:{
+            type: DataTypes.ARRAY(DataTypes.STRING),
+            allowNull: true, 
+        },
+        createdInDB: {
+            type: DataTypes.BOOLEAN, 
+            allowNull: false,
+            defaultValue: true, 
+        },
+    });
 };
