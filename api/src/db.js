8c15126d24b2352488ac159bdbf1e50bd035eb7d
Program222 <j_medina19@outlook.com>
2022-08-17 23:55:19 -0400

    db completed

--- api/src/db.js
+++ api/src/db.js
@@ -26,14 +26,17 @@ modelDefiners.forEach(model => model(sequelize));
 // Capitalizamos los nombres de los modelos ie: product => Product
 let entries = Object.entries(sequelize.models);
 let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
+
 sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Recipe } = sequelize.models;
+const { Recipes, Diets } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
+Recipes.belongsToMany(Diets,{through: 'diet_recipe'})
+Diets.belongsToMany(Recipes,{through: 'diet_recipe'})
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
